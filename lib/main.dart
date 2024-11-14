a1f9f52d0df788748bab02f2da62e36ec5660b52
= <=>
2024-11-14 15:30:02 +0200

    Refactor main function to initialize app with language selection and preferred orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 import 'Features/auth/sign_in_view.dart';
+import 'Features/select_language/presentation/manager/change_language_cubit/change_language_cubit.dart';
 import 'constant.dart';
+import 'core/database/cache_helper.dart';
 import 'core/services/service_locator.dart';
 
-void main() {
+Future<void> main() async {
   setup();
-  runApp(const Movie());
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await getIt<CacheHelper>().init();
+  runApp(
+    ChangeNotifierProvider(
+      create: (BuildContext context) => ChangeLanguageProvider()..getCachedLang(),
+      child: const Movie(),
+    ),
+  );
 }
 
 class Movie extends StatelessWidget {
